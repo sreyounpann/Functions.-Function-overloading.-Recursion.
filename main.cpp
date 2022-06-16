5f3d35d7c75e6eea26e7240dd9643b3a4a5ee4a3
Yeow Pann <83297826+sreyounpann@users.noreply.github.com>
2022-06-16 11:23:10 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,60 @@
+//
+//  main.cpp
+//  P12M23
+//
+//  Created by Yeow Pann on 16/6/22.
+//
+
+
+//Write a recursive function of finding the power of a number.
+//#include<iostream>
+//using namespace std;
+//int Pow(int basenum , int pownum)
+// {
+//    if (pownum==0) return 1;
+//    return Pow(basenum, pownum-1)*basenum;
+//
+//}
+// int main()
+// {
+//     int basenum,pownum;
+//     cout<<"--- FINDING POW NUMBER  USING RECURSIVE ---\n";
+//     cout<<"Enter Base number : "; cin>>basenum;
+//     cout <<"Enter Pow number : "; cin>>pownum;
+//     cout <<"-------------------------------------------\n";
+//     cout<<basenum <<" ^ " <<pownum <<" = "<< Pow(basenum,pownum)<< endl;
+// }
+
+
+//Write a recursive function, which displays N asterisks in a row; the N number is entered by the user. Give an example of function operation.
+//#include<iostream>
+//using namespace std;
+//void stars(int N){
+//    if (N == 0) return;
+//    cout << "* ";
+//    stars(N-1);
+//}
+//int main(){
+//    int n;
+//    cout<<"--- WELCOME TO THE STARS ---\n";
+//    cout<<"Input N : "; cin>>n;
+//    stars(n);
+//}
+
+//Write a recursive function, which computes the sum of all numbers in the range from a to b. The user enters a and b. Give an example of function operation.
+//#include<iostream>
+//using namespace std;
+//int sum(int A,int B )
+//{
+//    if (A==B) return A;
+//    return sum(A+1,B)+A;
+//}
+//int main(){
+//    int A;
+//    int B;
+//    cout <<"Input A:"; cin >> A;
+//    cout <<"Input B:"; cin >> B;
+//    cout <<sum(A,B);
+//}
+
+ 
